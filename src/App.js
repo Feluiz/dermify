512e8d047edf9f797f01a7fe92c6e33dede79a00
Feluiz <138745247+Feluiz@users.noreply.github.com>
2023-11-05 00:28:09 -0600

    First change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and change your life.
+          Edit <code>src/App.js</code> and change this life.
         </p>
         <a
           className="App-link"
