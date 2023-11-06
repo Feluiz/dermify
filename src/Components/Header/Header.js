6f7ab8aa88187266e9359776a6af0c729a21f694
Feluiz <138745247+Feluiz@users.noreply.github.com>
2023-11-06 01:29:50 -0600

    Added contact dropdown
    Installed react-bootstrap and extracted the dropdown dependencies, as well as jquery popper.js for styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,10 @@
+import { useState } from 'react';
 import './Header.css'
+
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/js/dist/dropdown';
 
 const Header = () => {
     let mainLogo = require("../../imgs/icons/logo-1.png");
@@ -6,7 +12,10 @@ const Header = () => {
     return (
         <div className='header-card'>
             <img className="main-logo" src={mainLogo} alt="main logo" />
-            <h2 className='contact'>Contacto</h2>
+            <DropdownButton id="dropdown-basic-button" title="Contacto">
+                <Dropdown.Item href="https://wa.me/9631283614">Comitán</Dropdown.Item>
+                <Dropdown.Item href="https://wa.me/9631300595">San Cristóbal</Dropdown.Item>
+            </DropdownButton>
         </div>
     )
 };
