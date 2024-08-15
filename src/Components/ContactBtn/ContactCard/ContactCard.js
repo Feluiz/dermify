697fb12369672249809ec29c6e630435c112368e
Luis Pérez <138745247+Feluiz@users.noreply.github.com>
2024-08-15 10:39:05 -0600

    style: Fixed issue regarding the section displayed on the item modal. (#4)
    * style: Fixed issue regarding the section displayed on the item modal.
    
    * perf: Added details on favicon and main html

--- src/Components/ContactBtn/ContactCard/ContactCard.js
+++ src/Components/ContactBtn/ContactCard/ContactCard.js
@@ -7,9 +7,6 @@ const ContactCard = () => {
             <a href='https://wa.me/529631283614' className='comitan-whatsapp'>
                 <p className='social-btn-txt'>Comitán</p>
             </a>
-            <a href='https://wa.me/529631300595' className='sancris-whatsapp'>
-                <p className='social-btn-txt'>San Cristóbal</p>
-            </a>
         </div>
     )
 };
